7db5e9e43b08f8672c99f9186dfedc6a24742e36
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-10-17 00:39:35 +0500

    added lexicon building script

--- Forward-Index/forwardIndex.py
+++ Forward-Index/forwardIndex.py
@@ -1,79 +1,15 @@
-# import os
-# import nltk
-# import json
-# from nltk.tokenize import word_tokenize, sent_tokenize
-# from nltk.probability import FreqDist
-# from nltk.corpus import stopwords
-# from nltk.stem import PorterStemmer, WordNetLemmatizer
-
-# class DocID_URL_Mapping():
-#     def __init__(self):
-#         self.documentIndexPath = ""
-#         self.mappings = self.loadDocumentIndex()
-        
-#     def addToDocumentIndex(self, docID, URL):
-#         if str(URL) not in self.mappings:
-#             self.mappings[docID] = URL
-#             print(f"Document ID : {docID} and the corresponding URL : {URL} added to the Document Index")
-#         else:
-#             print("Already Exists...!!")
-            
-#     def loadDocumentIndex(self):
-#         try:
-#             with open(self.documentIndexPath, "r") as file:
-#                 return json.load(file)
-#         except FileNotFoundError:
-#             return {}
-        
-#     def saveDocumentIndex(self):
-#         with open(self.documentIndexPath, "w") as file:
-#             json.dump(self.mappings, file, indent=2)
-            
-# class DocId_Date_Mapping():
-#     def __init__(self):
-#         self.documentIndexPath = ""
-#         self.mappings = loadDocIDDateFile()
-        
-#     def addDocumentIndexDataFile(self, DocID, date):
-#         if str(DocID) not in self.mappings:
-#             mappings[DocID] = date
-#             print(f"Document ID : {docID} and the corresponding Date : {date} added to the Document Index")
-#         else:
-#             print("Already Exists...!!")
-    
-#     def loadDocIDDateFile(self):
-#         try:
-#             with open(self.documentIndexPath, "r") as file:
-#                 return json.load(file)
-#         except FileNotFoundError:
-#             return {}
-        
-#     def saveDocIDDateFile(self):
-#         with open(self.documentIndexPath, "w") as file:
-#             json.dump(self.mappings, file, indent=2)
-            
-            
-# class Lexicon()
-
-
-# with open(r"dsa_data\abcnews.json", "r") as file:
-#     data = json.load(file)
-    
-# stopWords = set(stopwords.words('english'))
-
-# for article in data:
-#     title = article.get("title")
-#     content = article.get("content")
-#     wordToIndex = f"{title} {content}"
-#     tokens = word_tokenize(wordToIndex)
-#     freqDistribution = FreqDist(tokens)
-    
-import json
+import os
 import nltk
+import json
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+import re
 
 class DocID_URL_Mapping():
     def __init__(self):
-        self.documentIndexPath = r"A:\ProgrammingStuff\CS-250-Data-Structures-and-Algorithms\Forward-Index\URLs.json"
+        self.path = r"A:\ProgrammingStuff\CS-250-Data-Structures-and-Algorithms\Forward-Index\URLs.json"
         self.mappings = self.loadDocumentIndex()
         
     def addToDocumentIndex(self, docID, URL):
@@ -85,18 +21,18 @@ class DocID_URL_Mapping():
             
     def loadDocumentIndex(self):
         try:
-            with open(self.documentIndexPath, "r") as file:
+            with open(self.path, "r") as file:
                 return json.load(file)
         except FileNotFoundError:
             return {}
         
     def saveDocumentIndex(self):
-        with open(self.documentIndexPath, "w") as file:
+        with open(self.path, "w") as file:
             json.dump(self.mappings, file, indent=2)
         
 class DocID_Date_Mapping():
     def __init__(self):
-        self.documentIndexPath = r"A:\ProgrammingStuff\CS-250-Data-Structures-and-Algorithms\Forward-Index\Dates.json"
+        self.path = r"A:\ProgrammingStuff\CS-250-Data-Structures-and-Algorithms\Forward-Index\Dates.json"
         self.mappings = self.loadDocumentIndex()
         
     def addToDocumentIndex(self, docID, date):
@@ -108,26 +44,67 @@ class DocID_Date_Mapping():
             
     def loadDocumentIndex(self):
         try:
-            with open(self.documentIndexPath, "r") as file:
+            with open(self.path, "r") as file:
                 return json.load(file)
         except FileNotFoundError:
             return {}
         
     def saveDocumentIndex(self):
-        with open(self.documentIndexPath, "w") as file:
+        with open(self.path, "w") as file:
             json.dump(self.mappings, file, indent=2)
         
-        
-        
-with open(r"dsa_data/activistpost.json", "r") as file:
-    data = json.load(file)
 
+lemmatizer = WordNetLemmatizer()
+            
+def preprocess(content):
+    content = content.replace("\n", " ")
+    content = content.replace("\t", " ")
+    content = re.sub(r"[^A-Za-z\s]", " ", content)
+    content = re.sub(r"\s+", " ", content)
+    content = content.lower()
+    content = word_tokenize(content)
+    content = [lemmatizer.lemmatize(word) for word in content if word not in stopwords.words("english") and len(word) > 2]
+    return content
+            
+with open(r"A:\ProgrammingStuff\dsa_data\activistpost.json", "r") as file:
+    data = json.load(file)
     
-urlmapper = DocID_URL_Mapping()
-datemapper = DocID_Date_Mapping()
-for dat in data:
-    urlmapper.addToDocumentIndex(dat["id"], dat["url"])
-    datemapper.addToDocumentIndex(dat["id"], dat["date"])
+print(len(data))
+
+def lexiconBuilder(words):
+    try:
+        with open(r"A:\ProgrammingStuff\CS-250-Data-Structures-and-Algorithms\Forward-Index\Lexicon.json", "r") as file:
+            IDs = json.load(file)
+            IDnumber = max(IDs.values()) + 1 if IDs else 1
+    except:
+        IDs = {}
+        IDnumber = 1
     
-urlmapper.saveDocumentIndex()
-datemapper.saveDocumentIndex()
+    for word in words:
+        if word not in IDs:
+            IDs[word] = IDnumber
+            IDnumber += 1
+    
+    with open(r"A:\ProgrammingStuff\CS-250-Data-Structures-and-Algorithms\Forward-Index\Lexicon.json", "w") as file:
+        json.dump(IDs, file, indent=2)
+
+# def buildForwardIndex(documents):
+#     for document in documents:
+#         for article in document:
+#             title = preprocess(article["title"])
+#             content = preprocess(article["content"])
+#             lexiconBuilder(title + content)
+#             try:
+#                 with open(r"A:\ProgrammingStuff\CS-250-Data-Structures-and-Algorithms\Forward-Index\Lexicon.json", "r") as file:
+#                     lexicon = json.load(file)
+#             except FileNotFoundError:
+#                 return {}
+            
+#             title_ids = [lexicon[word] for word in title]
+#             content_ids = [lexicon[word] for word in content]
+            
+#             print(title_ids)
+            
+# buildForwardIndex([data])
+            
+            
