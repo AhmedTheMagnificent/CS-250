94573368829fd1898d3855f0f2299f71df5d9262
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-11-22 00:50:52 +0500

    added complete inverted index

--- Inverted-Index/invertedIndex.py
+++ Inverted-Index/invertedIndex.py
@@ -10,21 +10,34 @@ barrelFiles = []
 with open(r"A:\ProgrmmingStuff\CS-250-Data-Structures-and-Algorithms\Forward-Index\ForwardIndex.json", "r") as file:
     forwardIndex = json.load(file)
     
-def buildInvertedIndex(forwardIndex, lexicon, invertedIndex):
+def buildInvertedIndex(forwardIndex):
     barrels = [None] * NUM_BARRELS
-    for document in forwardIndex:
-        documentID = document.key()
-        for wordID, frequency in document.items():
-            invertedIndex[wordID] = {documentID: frequency}
-            barrel_number = int(wordID % NUM_BARRELS)
+    for documentID, data in forwardIndex.items():
+
+        for wordID, frequency in data.items():
+            barrel_number = int(wordID) % NUM_BARRELS
             barrel_filename = f"barrel_{str(barrel_number).zfill(4)}.json"
             barrel_path = os.path.join(r"A:\ProgrmmingStuff\CS-250-Data-Structures-and-Algorithms\Barrels", barrel_filename)
+            print(barrel_number)
             if not os.path.exists(barrel_path):
                 with open(barrel_path, "w") as file:
                     json.dump({}, file)
                 barrelFiles.append(barrel_filename)
                 
             
-    
-    
-print(data)
+                
+            if barrels[barrel_number] is None:
+                with open(barrel_path, "r+") as file:
+                    barrels[barrel_number] = json.load(file)
+                
+            if wordID not in barrels[barrel_number]:
+                barrels[barrel_number][wordID] = {}
+            barrels[barrel_number][wordID].update({documentID: frequency})
+            
+            with open(barrel_path, "w") as file:
+                json.dump(barrels[barrel_number], file)
+            
+                
+            
+
+buildInvertedIndex(forwardIndex)
